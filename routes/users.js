d3ac02f55bbbfe1ddb7e1ce2d69b752c96abb07c
alluans <alluans@yandex.ru>
2018-02-19 14:01:17 +0200

    initial server

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/fake', function(req, res, next) {
+    res.send('fake');
+});
+
 module.exports = router;
