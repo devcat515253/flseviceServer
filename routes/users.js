7da050207717fc24e88bdc397034df123b63ff3a
alluans <alluans@yandex.ru>
2018-02-19 15:03:49 +0200

    initial server

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var sequelize = require('../db-connection');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+
+
+    sequelize.query(`SELECT * FROM flservice.user`,
+        { type: sequelize.QueryTypes.SELECT })
+        .then(users => {
+            res.send(users);
+
+        }).catch(error =>{
+            throw new Error(error);
+        });
+
+
 });
 
 router.get('/fake', function(req, res, next) {
